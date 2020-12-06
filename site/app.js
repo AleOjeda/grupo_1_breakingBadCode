8586f0bf1d34371585d7f83fb0093c719305141a
Alejandro Ojeda <ojeda.alejandro@hotmail.com>
2020-12-05 23:17:47 -0300

    Reorganización carpetas + Server On

--- site/app.js
+++ site/app.js
@@ -3,3 +3,19 @@ const express = require('express');
 
 //Iniciar App:
 const app = express();
+
+//Archivos estaticos
+app.use(express.static('public'));
+
+
+//Probando servidor localhost
+app.get('/', (req,res) =>{
+    res.send('Hello World');
+})
+
+//Iniciando en puerto 3000.
+app.listen(3000, () => {
+    console.log('Corriendo en puerto 3000');
+    console.log();
+    console.log('visitar http://localhost:3000');
+});
