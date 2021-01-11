092f6a5ec5de3640e88d416501df062ff1ad21e4
Alejandro Ojeda <ojeda.alejandro@hotmail.com>
2021-01-11 02:54:56 -0300

    herokuapp

--- site/app.js
+++ site/app.js
@@ -11,13 +11,11 @@ app.use(express.static('public'));
 //Probando servidor localhost
 app.get('/', (req,res) =>{
     res.send('Hello World');
-})
-
+});
 //Inicio de Herokuapp
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Servidor corriendo en el puerto 3000`);
-   })
-
+   });
 
 //Iniciando en puerto 3000.
 app.listen(3000, () => {
@@ -25,3 +23,8 @@ app.listen(3000, () => {
     console.log();
     console.log('visitar http://localhost:3000');
 });
+
+
+
+
+
