fbea3c7bba55e20d2971705bc8b849c7dd22f8ec
Alejandro Ojeda <ojeda.alejandro@hotmail.com>
2021-01-15 19:49:20 -0300

    organización de carpetas

--- site/app.js
+++ site/app.js
@@ -13,10 +13,10 @@ app.get('/', (req,res) =>{
     res.send('Hello World');
 });
 //Inicio de Herokuapp
-app.listen(process.env.PORT || 3000, function() {
+/*  app.listen(process.env.PORT || 3000, function() {
     console.log(`Servidor corriendo en el puerto 3000`);
    });
-
+ */ 
 //Iniciando en puerto 3000.
 app.listen(3000, () => {
     console.log('Corriendo en puerto 3000');
