2159e1ca0fb43d936d10f142b9aaaed973e27644
Alejandro Ojeda <ojeda.alejandro@hotmail.com>
2021-02-06 20:23:48 -0300

    antes de empezar a dividir los controladores y middlewares

---
+++ site/src/controllers/productsController.js
@@ -0,0 +1,4 @@
+// Listar productos por cátegoria
+// Listar productos por subCategoría
+// Buscar productos
+// Agregar productos al carrito
--- site/src/routes/mainRouter.js
+++ site/src/routes/mainRouter.js
@@ -1,3 +1,25 @@
+//dividir el router entre el router de todo lo que tenga que ver con usuarios.
+//registro inicio (luego será cambiar dirección etc etc)
+// editar cosas del usuario, mostrar sus datos /:id
+///www.qwerty.com/users/:id por ejemplo. o todas las opciones del usuario. 
+//(no se si se haran)
+
+
+//router de producto
+//pagina de producto 
+//categorias www.qweryy.com/:idproducto1
+
+//tendriamos que tener un /categorias que tendra la opcion 
+//de ver los productos de categoria? sub categoria etc? www.qwerty.com/categoria1
+
+
+
+
+
+
+//en el main router tiene q ir el /contacto / quienes somos /home
+
+
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/mainController')
