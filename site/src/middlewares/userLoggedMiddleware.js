9fc3703e4670b17dc1848998a1143c0a25d49524
Alejandro Ojeda <ojeda.alejandro@hotmail.com>
2021-03-07 04:26:14 -0300

    Fix bug de userLoggedMiddleware -> Si no tiene cookie guardada, next. Si no, recupera el email

--- site/src/middlewares/userLoggedMiddleware.js
+++ site/src/middlewares/userLoggedMiddleware.js
@@ -1,3 +1,46 @@
+const db = require('../database/models');
+const jsonTable = require('../database/jsonTable');
+const usersTable = jsonTable('users');
+
+function userLoggedMiddleware (req,res,next){
+    res.locals.isLogged = false;
+    //Si, tiene un email registrado en el cookies, ejecuta el if, si no, else y next().
+    if(req.cookies.userEmail) {
+        let emailInCookie = req.cookies.userEmail;
+        db.Users.findOne({
+            where: {
+                email: emailInCookie
+            }
+        })
+        .then (userFromCookie =>{
+            if(userFromCookie.length != 0){
+                req.session.userLogged = userFromCookie;
+            };
+            if(req.session.userLogged){
+                res.locals.isLogged = true;
+                res.locals.userLogged = req.session.userLogged;
+            }
+            next()
+        })
+    } else {
+        next()
+    }
+//next()
+}
+
+module.exports = userLoggedMiddleware;
+
+/* db.Users.findOne({
+    where: {
+        email: req.body.email
+    }
+})
+.then */
+
+
+// antes de tocar algo
+/* 
+const db = require('../database/models');
 const jsonTable = require('../database/jsonTable');
 const usersTable = jsonTable('users');
 
@@ -20,4 +63,4 @@ function userLoggedMiddleware (req,res,next){
     //Preguntar porque uso dos If si podria haber sido solo 1.
 }
 
-module.exports = userLoggedMiddleware;
+module.exports = userLoggedMiddleware; */
